9e3f60a904974b4dd889c949b5efdd490afab6d9
wyan1103 <55310635+wyan1103@users.noreply.github.com>
2020-05-22 22:14:13 -0400

    Add files via upload
    added bot initial startup

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Botkic
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        static void Main(string[] args) => 
+            new Program().RunAsync().GetAwaiter().GetResult();
+
+        // initial setup on bot login
+        public async Task RunAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "NzEyNzUxOTc5Mjg1MzE1NjU0.XsWIJA.EJufMszNo1pKFfyegEvXuJn7tAA";
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+        // error logs
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        // handle commands given to the bot with prefix "."
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var  msg = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, msg);
+            if(msg.Author.IsBot) return;
+
+            int argPos = 0;
+            if(msg.HasStringPrefix(".", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
